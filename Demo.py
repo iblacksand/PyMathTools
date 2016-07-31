e12fe371ac34389b63e3a33613382246af2a96a6
john elizarraras <john elizarraras>
2016-07-30 22:02:25 -0600

    adds the read_file demo

--- Demo.py
+++ Demo.py
@@ -1,6 +1,4 @@
-from PyMathTools import mod
-from PyMathTools import binary_search
-
+from PyMathTools import *
 # binary_search
 a = [6,7,8,9,10]
 print(binary_search(a, 8)) # returns 2
@@ -9,3 +7,4 @@ print(binary_search(a, 8)) # returns 2
 print('34 mod 7 = ' + str(mod(34,7))) # returns 6
 print('1/4 mod 7 = ' + str(mod(1/4,7))) # returns 2
 # print('4 mod 5.5  = ' + str(PyMathTools().mod(4, 5.5))) - this would throw an exception
+print(read_file("nums.txt"))
